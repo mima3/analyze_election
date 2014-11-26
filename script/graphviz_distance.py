f4a4ab921a6d7a35d0dceffe36c40af672770e36
m.itagaki2 <mima@js6.so-net.ne.jp>
2014-11-26 23:02:22 +0900

    ドント式の計算モジュール追加 PDFの政策集の解析処理追加

---
+++ script/graphviz_distance.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import pydot
+
+def draw_distance(distance_result, output_file):
+  cnt = 0
+  sum = 0.0
+  for f,dic in distance_result.items():
+    max = -99999.99
+    min = 9999.99
+    max_node = ""
+    min_node = ""
+    
+    for t, d in dic.items():
+      print "%s -> %s : %f" % (f,t,d)
+      if max < d:
+        max = d
+        max_node = t
+      if min > d:
+        min = d
+        min_node = t
+      cnt = cnt + 1
+      sum = sum + d
+    print "....... max:%s(%f) min:%s(%f)" % (max_node,max,min_node,min)
+
+  avg = sum / cnt
+  print "average %f" % (avg)
+  graph = pydot.Dot(graph_type='graph')
+  nodes = {}
+  for f,dic in distance_result.items():
+    nodes[f] = pydot.Node(f, label=f, style="filled", fillcolor="white",fontname = "ms ui gothic", shape="box")
+    graph.add_node(nodes[f])
+
+  records = []
+  for f,dic in distance_result.items():
+    for t, d in dic.items():
+      nf = nodes[f]
+      nt = nodes[t]
+      if ( t + f ) in records:
+        graph.add_edge(pydot.Edge(nf, nt,len=(d/avg*10)))
+        print "%s - %s - %f" % (f,t,d/avg*10)
+      records.append( f + t )
+  graph.write_png(output_file,prog='neato')
---
+++ script/tf_idf_result.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from collections import defaultdict
+
+class tf_idf_result:
+  """
+  tf-idf‚ÌƒXƒRƒA‚ÌŒ‹‰Ê‚ğŠi”[‚·‚é
+  """
+  def __init__(self,category):
+    self.category = category
+    self.term_scores = defaultdict(int)
+
+  def set_score(self,term,score):
+    self.term_scores[term] = score
+
