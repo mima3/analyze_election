4c2a058919de205c4f72f884f767fe90563faaf1
m.itagaki2 <mima@js6.so-net.ne.jp>
2014-11-27 13:12:15 +0900

    ドント式のページのひな形作成 imagesフォルダの移動

---
+++ js/dondt.js
@@ -0,0 +1,59 @@
+$(function() {
+  var lCurEditRow;
+  var lCurEditCol;
+  function calc_dondt(successFunc)
+  {
+    var max_seats = $("#max_seats").val();
+    $("#votes").restoreCell(lCurEditRow,lCurEditCol);
+    var vote_data = $("#votes").getRowData();
+    $.ajax({
+      type:'POST',
+      url: 'main.py?CalcDondt',
+      data: { max_seats:max_seats,vote_data:JSON.stringify(vote_data)},
+      dataType:'json',
+      success: function(json) {
+        vote_data=json;
+        $("#votes").clearGridData();
+        $("#votes").addRowData("1",vote_data);
+      },
+      complete:function(json) {
+      },
+      error: function(xhr, textStatus, errorThrown){
+        alert('Failed CalcDondt.' );
+      }
+    });
+  }
+
+  $(document).ready(function()
+  {
+    var vote_data = [];
+    $("#calc_dondt").button().click(function(event){
+      event.preventDefault();
+      calc_dondt( function() {} );
+    });
+    $("#votes").jqGrid({
+      data:vote_data,
+      datatype:"local",
+      colNames:["政党","立候補者数","投票数","議席"],
+      colModel:[
+        {name:'name'},
+        {name:'max', editable:true,edittype:'text',align:'right',editrules:{integer:true}, formatter:'integer',sorttype:'int'},
+        {name:'votes', editable:true,edittype:'text',align:'right',editrules:{integer:true}, formatter:'integer',sorttype:'int'},
+        {name:'seats',align:'right',sorttype:'int'},
+      ],
+      height:'100%',
+      width:'100%',
+      multiselect: false,
+      caption: 'ドント式比例議席数',
+      afterSaveCell: function(rowid, cellname,value,iRow,iCol){
+      },
+      beforeEditCell: function(rowid,cellname,value,iRow,iCol) {
+        lCurEditRow = iRow;
+        lCurEditCol = iCol;
+        
+      },
+      cellEdit:true,
+      cellsubmit:'clientArray',
+    });
+  });
+});
---
+++ views/home.tpl
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html lang="ja">
+<head>
+  <title>選挙情報</title>
+  <link rel="stylesheet" href="/analyze_election/js/jquery/jquery-ui.min.css" type="text/css" />
+  <link rel="stylesheet" href="/analyze_election/base.css" type="text/css" />
+</head>
+<body>
+  <div id="contents">
+    <h1>選挙情報</h1>
+    <p><a href="/analyze_election/page/ElectionArea">小選挙区の区割り情報</a></p>
+  </div>
+</body>
+</html>
