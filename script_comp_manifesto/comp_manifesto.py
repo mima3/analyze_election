62794142362ac522ec3c928c559f73fa6a7792f1
m.itagaki2 <mima@js6.so-net.ne.jp>
2014-12-02 21:54:16 +0900

    JSONで政党をしていするように変更。

--- script_comp_manifesto/comp_manifesto.py
+++ script_comp_manifesto/comp_manifesto.py
@@ -8,7 +8,7 @@ import nltk
 from pdf_to_text import http_pdf_to_text
 import calc_distance
 import graphviz_distance
-from json import dumps
+import json
 import urllib2
 import lxml.html
 import codecs
@@ -95,31 +95,39 @@ def get_party_content(name, url):
 
 
 def main(argvs, argc):
-    if(argc != 4):
-        print ("Usage #python %s output_imagefile output_jsonfile font_path" % argvs[0])
+    if(argc != 5):
+        print ("Usage #python %s output_imagefile input_jsonfile output_jsonfile font_path" % argvs[0])
         print ('font path : ex "ms ui gothic" or "/home/fonts/ipca.ttc"')
         return -1
-    output_imagefile = argvs[1]
-    output_jsonfile = argvs[2]
-    font_path = argvs[3]
+    input_jsonfile = argvs[1]
+    output_imagefile = argvs[2]
+    output_jsonfile = argvs[3]
+    font_path = argvs[4]
 
-    partys = {
-        "自民党": "http://jimin.ncss.nifty.com/pdf/news/policy/126585_1.pdf",
-        "公明党": "http://www.komei.or.jp/campaign/shuin2014/manifesto/manifesto2014.pdf",
-        "民主党": "http://www.dpj.or.jp/global/downloads/manifesto2014.pdf",
-        "維新の党": "https://ishinnotoh.jp/activity/news/2014/11/23/20141122-seisaku.pdf",
-        #"次世代の党": "http://jisedai.jp/cp-bin/wordpress/wp-content/uploads/2014/11/%EF%BC%88%E6%94%BF%E7%AD%96%E9%9B%86%EF%BC%89%E6%AC%A1%E4%B8%96%E4%BB%A3%E3%81%8C%E8%AA%87%E3%82%8A%E3%82%92%E6%8C%81%E3%81%A6%E3%82%8B%E6%97%A5%E6%9C%AC%E3%82%92.pdf",
-        #"共産党": "http://www.jcp.or.jp/web_download/seisaku/201307_seisaku_panf.pdf",
-        "生活の党": "http://www.seikatsu1.jp/wp-content/uploads/0c4778a35f0cfe0a34fd3085e210a5c4.pdf"
-        #"社民党": "http://www5.sdp.or.jp/policy/policy/election/2013/data/digest.pdf" # 公示後
-    }
+    #partys = {
+    #    "自民党": "http://jimin.ncss.nifty.com/pdf/news/policy/126585_1.pdf",
+    #    "公明党": "http://www.komei.or.jp/campaign/shuin2014/manifesto/manifesto2014.pdf",
+    #    "民主党": "http://www.dpj.or.jp/global/downloads/manifesto2014.pdf",
+    #    "維新の党": "https://ishinnotoh.jp/activity/news/2014/11/23/20141122-seisaku.pdf",
+    #    #"次世代の党": "http://jisedai.jp/cp-bin/wordpress/wp-content/uploads/2014/11/%EF%BC%88%E6%94%BF%E7%AD%96%E9%9B%86%EF%BC%89%E6%AC%A1%E4%B8%96%E4%BB%A3%E3%81%8C%E8%AA%87%E3%82%8A%E3%82%92%E6%8C%81%E3%81%A6%E3%82%8B%E6%97%A5%E6%9C%AC%E3%82%92.pdf",
+    #    #"共産党": "http://www.jcp.or.jp/web_download/seisaku/201307_seisaku_panf.pdf",
+    #    "生活の党": "http://www.seikatsu1.jp/wp-content/uploads/0c4778a35f0cfe0a34fd3085e210a5c4.pdf"
+    #    #"社民党": "http://www5.sdp.or.jp/policy/policy/election/2013/data/digest.pdf" # 公示後
+    #}
+
+    f = open(input_jsonfile, 'r')
+    partys = json.load(f)
+    f.close()
+
 
     docs = []
     effective_partys = []
     for k, v in partys.items():
         print (u"%sを処理中..." % (k))
         text = get_party_content(k, v)
-        if text is None:
+        if (not text):
+            continue
+        if (not text.strip()):
             continue
         docs.append(get_tokens(text))
         effective_partys.append(k)
@@ -158,7 +166,7 @@ def main(argvs, argc):
         i = i + 1
 
     f = open(output_jsonfile, 'w')
-    f.write(dumps(output_json))
+    f.write(json.dumps(output_json))
     f.close()
 
     distance_result = calc_distance.calc_distance(results)
