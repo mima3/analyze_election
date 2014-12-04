6bde8354763b58ddda8f49809df713ee29bf8ac8
m.itagaki2 <mima@js6.so-net.ne.jp>
2014-12-04 17:02:21 +0900

    ４７回に変更

--- script/analyze_asahi.py
+++ script/analyze_asahi.py
@@ -11,7 +11,7 @@ def print_area(url):
     r = urllib2.urlopen(url, timeout=30)
     html = r.read()
     dom = lxml.html.fromstring(html)
-    areas = dom.xpath('//div[@class="H2Box snkH2Box"]/h2')
+    areas = dom.xpath('//div[@class="snkH2Box"]/h2')
     tables = dom.xpath('//table[@class="snkTbl01"]')
     for i in range(0, len(areas)):
         h2 = areas[i].text_content().encode('utf-8')
@@ -29,9 +29,9 @@ def main(argvs, argc):
     """
     このスクリプトでは、朝日新聞の情報から小選挙区の立候補者を取得します
     """
-    url = 'http://www.asahi.com/senkyo/sousenkyo46/kouho/A01.html'
+    url = 'http://www.asahi.com/senkyo/sousenkyo47/kouho/A01.html'
     for i in range(1, 48):
-        url = ('http://www.asahi.com/senkyo/sousenkyo46/kouho/A%s.html' % str(i).zfill(2))
+        url = ('http://www.asahi.com/senkyo/sousenkyo47/kouho/A%s.html' % str(i).zfill(2))
         print_area(url)
 
 if __name__ == '__main__':
